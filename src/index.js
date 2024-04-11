c09befaea2b81a8e1c6ba4146b096669d25eaed4
pradiptechie <kumarpradip3956@gmail.com>
2024-04-11 07:44:36 +0545

    Things I learned added in Readme.md

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-                                                                                                                                                                           
+  // import React from "react";
+  // import ReactDom from 'react-dom';
+
+  // import App from './App';
+
+  // ReactDom.render(<App/>, document.getElementById('root'))
+
+  import React from 'react';
+  import ReactDOM from 'react-dom/client';
+  import App from './App';
+  import reportWebVitals from './reportWebVitals';
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+  reportWebVitals();
